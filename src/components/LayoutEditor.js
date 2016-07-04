59b5bd67bbf116a6e030ef8232756106356dec8d
Philippe Guay <Philippe.Guay@videri.com>
2016-07-03 22:06:46 -0400

    Clean up unused components

--- src/components/LayoutEditor.js
+++ src/components/LayoutEditor.js
@@ -11,10 +11,8 @@ class LayoutEditor extends Component {
     this.handleFeedOrderChange = this.handleFeedOrderChange.bind(this)
   }
 
-
   handleTweetLimitChange(event) {
-      console.log('handleTweetLimitChange', event.target.value)
-      this.setState({tweetLimit: parseInt(event.target.value)})
+    this.setState({tweetLimit: parseInt(event.target.value)})
   }
 
   handleThemeChange(theme) {
@@ -26,20 +24,20 @@ class LayoutEditor extends Component {
   }
 
   render() {
-    const { isEditing, tweetLimit, columnsOrder, theme,  onSave} = this.props
+    const { isEditing, tweetLimit, columnsOrder, theme,  onSave, onCancel} = this.props
     return (
       <div id="layout-editor">
         <div className="row">
-          <input type="number" onChange={this.handleTweetLimitChange } value={this.state.tweetLimit} min="0"/>
+          <label for="limit">Tweet Limit</label>
+          <input type="number" id="limit" onChange={this.handleTweetLimitChange } value={this.state.tweetLimit} min="0"/>
         </div>
         <div className="row">
-          <Picker value={this.state.theme} onChange={this.handleThemeChange} options={themes} />
-        </div>
-        <div className="row">
-          <Picker value={columnsOrder[0]} onChange={this.handleFeedOrderChange} options={feeds} />
+          <label for="picker">Theme</label>
+          <Picker id="picker" value={this.state.theme} onChange={this.handleThemeChange} options={themes} />
         </div>
         <div className="row">
           <a className="btn btn-primary" href="#" onClick={e=> {onSave(this.state)}}>Save</a>
+          <a className="btn btn-default" href='#' onClick={e=> onCancel(e)}>Cancel</a>
         </div>
       </div>
     )
@@ -50,7 +48,8 @@ LayoutEditor.propTypes = {
   isEditing: PropTypes.bool.isRequired,
   tweetLimit: PropTypes.number.isRequired,
   columnsOrder: PropTypes.array,
-  onSave: PropTypes.func.isRequired
+  onSave: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired
 }
 
 export default LayoutEditor
